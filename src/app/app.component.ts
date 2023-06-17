9aeabad7b04c0e6d6c5e48fbc940c81f1d691673
Chris <chris@iijat.com>
2023-06-17 19:28:07 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'delegation.monster-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'delegation.monster';
+}
